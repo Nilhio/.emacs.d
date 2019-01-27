6c89944435e40ac8456fcbd4a5078c79e3288740
Olgmir <edvinas.kurmanavicius@gmail.com>
2019-01-27 18:56:41 +0200

    comment out missing org dependency * core/init-org-mode.el:

--- core/init-org-mode.el
+++ core/init-org-mode.el
@@ -12,11 +12,11 @@
   :bind (("C-c c" . org-capture))
 )
 
-(use-package org-sticky-header
-  :config
-  (setq-default
-   org-sticky-header-full-path 'full
-   org-sticky-header-outline-path-separator " / "))
+;; (use-package org-sticky-header
+;;   :config
+;;   (setq-default
+;;    org-sticky-header-full-path 'full
+;;    org-sticky-header-outline-path-separator " / "))
 
 (use-package toc-org :after org)
 
